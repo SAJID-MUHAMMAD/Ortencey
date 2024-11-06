06c7afbe7fc95c2e522135a05201e640049dab09
SAJID 23.0 <mdsajid1375@gmail.com>
2024-11-06 23:37:07 +0600

    delete

--- src/Components/Home/About.jsx
+++ src/Components/Home/About.jsx
@@ -29,6 +29,22 @@ const About = () => {
               <div>
                 <span>SEO Optimization</span>
                 <span>First Working Process</span>
+
+                {/* ============== =================================*/}
+                <h1>heoerusbdfkjiejlanfldskfdsfkfdsl</h1>
+                <h3 className="text-xl font-dmsans font-medium text-[#3661FC]">
+                  About Our Company
+                </h3>
+                <h2>Best Digital Marketing Agency For People</h2>
+                <p>
+                  There are many variations of passages of Lorem Ipsum
+                  available, but the majority have as suffered alteration in
+                  some form, by injected humour, or randomised words which don't
+                  a look even slightly believable. If you are going to use a
+                  passage of Lorem Ipsum, you need to be sure there isn't
+                  anything.
+                </p>
+                {/* =============================================== */}
               </div>
             </div>
           </div>
